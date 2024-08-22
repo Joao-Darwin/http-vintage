c928d24752c6da715d8cdb30b416fa7539172949
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-22 12:32:45 -0300

    feat: Criar estrutura básica das rotas, bem como cabeçalho e rodapé

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <h1>Header</h1>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <h1>Footer</h1>
+    </BrowserRouter>
   )
 }
 
