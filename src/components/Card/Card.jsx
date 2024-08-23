e74a556719c876f756f0721350fa8d0c8a6b1874
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-23 16:57:51 -0300

    feat: Adicionar navegação para página de status quando clicar no card, mandando código através da rota

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import "./Card.css";
 
 const Card = ({ image, statusCode }) => {
+    const nativate = useNavigate();
+
     return (
-        <div className="card">
+        <div className="card" onClick={() => nativate(`/status/${statusCode}`)}>
             <img className="image" src={image} alt={`Status code ${statusCode} image`} />
             <div className="divStatusCode">
                 <h2>{statusCode}</h2>
