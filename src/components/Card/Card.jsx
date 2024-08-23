232a1fbf79e851f978fd5f5f766bba18285af67d
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-23 19:13:55 -0300

    feat: Adicionar título do código http no card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,14 +1,14 @@
 import { useNavigate } from "react-router-dom";
 import "./Card.css";
 
-const Card = ({ image, code }) => {
+const Card = ({ image, code, title }) => {
     const nativate = useNavigate();
 
     return (
         <div className="card" onClick={() => nativate(`/status/${code}`)}>
             <img className="image" src={image} alt={`Status code ${code} image`} />
             <div className="divCode">
-                <h2>{code}</h2>
+                <h2>{code} {title}</h2>
             </div>
         </div>
     )
